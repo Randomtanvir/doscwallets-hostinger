fad7fe0e2ee5951d32bebb4cf94d5d6a059956dd
Tanvir <tanvir.info07@gmail.com>
2025-12-05 05:23:51 +0600

    chabge setting

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: "build/.next", // project root এর ভিতরে রাখুন
+  distDir: "build/.next", // project root এর ভিতরে
+  experimental: {
+    turbo: false, // Turbopack disable
+  },
 };
 
 export default nextConfig;
