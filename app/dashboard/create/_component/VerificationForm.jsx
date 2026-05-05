af88207d1db3d709f3867617744ed67bf26282ea
Tanvir <tanvir.info07@gmail.com>
2026-05-06 00:45:59 +0600

    verificationFORMupdate

--- app/dashboard/create/_component/VerificationForm.jsx
+++ app/dashboard/create/_component/VerificationForm.jsx
@@ -13,17 +13,17 @@ export default function VerificationForm({
   const [loading, setLoading] = useState(false);
   const { register, handleSubmit, reset } = useForm({
     defaultValues: {
-      transactionNumber: "VN00513",
-      paymentId: "202528641492",
+      transactionNumber: "VN00527",
+      paymentId: "202528641429",
       totalPayment: "OMR 25.75",
-      transactionDate: "10 APR 2026",
+      transactionDate: "10 MAY 2026",
       documentType: "Marriage certificate",
       applicantName: "",
       email: "@gmail.com",
-      phoneNumber: "78973",
+      phoneNumber: "78951",
       verifierName: "Foreign Ministry - Oman",
       verificationStatus: "Approved",
-      verificationDateTime: "2026-04-10 11:07:53",
+      verificationDateTime: "2026-05-10 11:03:14",
       urlLink: verifactionData?.urlLink || generateEncodedString(),
     },
   });
